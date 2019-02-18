8dadf862376c166d3b555a119e0d77d77dc528f1
lihanhui <lihanhui@lihanhuideMacBook-Air.local>
2019-02-18 09:28:24 +0800

    detab

--- src/wrapper/wrapper.cxx
+++ src/wrapper/wrapper.cxx
@@ -6,9 +6,9 @@
 using namespace aquaman::xlog;
 
 bool logger::log_enabled(log_level level){
-	return true;
+    return true;
 }
 
 /*template<class... Args> logger & logger::log(log_level level, std::string && file, int line, std::string_view fmt, const Args&... args){
-	return *this;
+    return *this;
 }//*/
