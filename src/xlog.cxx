9a0a2ea247d6aa3cff02c7064058ef06e9ff3464
lihanhui <lihanhui@lihanhuideMacBook-Air.local>
2019-02-18 09:21:24 +0800

    detab

--- src/xlog.cxx
+++ src/xlog.cxx
@@ -4,13 +4,13 @@
 #include <xlog/xlog.h>
 int main(){
     using namespace  aquaman::xlog;
-   logger logger_ = logger("main");
+    logger logger_ = logger("main");
     LOG(logger_, log_level::DEBUG, "{}", "this is main func of xlog test");
     
     while(true){
-    	using namespace std::chrono_literals;
-    	std::this_thread::sleep_for(500ms);
-    	LOG(logger_, log_level::DEBUG, "{}", "this is log in main loop of xlog test");
+        using namespace std::chrono_literals;
+        std::this_thread::sleep_for(500ms);
+        LOG(logger_, log_level::DEBUG, "{}", "this is log in main loop of xlog test");
     }
     return 0;
 }
